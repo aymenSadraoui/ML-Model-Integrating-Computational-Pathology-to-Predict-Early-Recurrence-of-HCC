ed9c77f9618db2773316f81319e8ca85e1cfb59b
aymenSadraoui <aymensadraoui1919@gmail.com>
2025-12-16 12:49:59 +0000

    update bash script for step3

--- scripts/run_step3_bis.sh
+++ scripts/run_step3_bis.sh
@@ -1,7 +1,6 @@
 python src/STEP0_create_directories.py
-
-wsi_path="data/patches_clr/*"
-for wsi in $wsi_path; do
+# reversed order
+for wsi in $(ls -d data/patches_bis/* | sort -r); do
     echo "processing $wsi"
     python src/STEP3_detect_inflammatory_cells.py --slide_name "$wsi"
 done
